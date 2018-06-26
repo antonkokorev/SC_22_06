02bf3117118514799095df4d04735105aa29f02f
MichaelWebHub <michael.kutateladze@yandex.ru>
2018-06-26 12:23:57 +0300

    fixed swiper

--- Root Folder/_HR/Smart/tech1/modules/scMainBody.js
+++ Root Folder/_HR/Smart/tech1/modules/scMainBody.js
@@ -21,108 +21,108 @@ function classMainBody() {
     //this.currentUser="Basic Z2VvcmdpZXYtZWk6cXdlcnR5MTIz",
     this.currentUser = "Basic ZG9tb3poYWtvX212OjEyMzQ1VGdi",
         that_ = this;
-        that_.user="Krylova-YV";
-        this.ajax = function (param) {
-            /*var that=this;*/
-            //чтобы удобнее собирать параметры было
-            if(param.params_){
-                param.params="";
-               for(key in param.params_)
-               {
-                   param.params+="&"+key+"="+param.params_[key]
-               }
+    that_.user = "Krylova-YV";
+    this.ajax = function (param) {
+        /*var that=this;*/
+        //чтобы удобнее собирать параметры было
+        if (param.params_) {
+            param.params = "";
+            for (key in param.params_) {
+                param.params += "&" + key + "=" + param.params_[key]
             }
+        }
 
-            $.ajax
-            ({
-                type: "GET",
-                url: param.url + param.params,
-                dataType: 'jsonp',
-                name: key,
-                async: true,
-                jsonpCallback: param.callback,
-                //headers:{"Authorization":"Basic ZG9tb3poYWtvX212OjEyMzQ1VGdi"},
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("Authorization", "Form " + btoa("domozhako_mv:12345Tgb")/*"Basic ZG9tb3poYWtvX212OjEyMzQ1VGdi"*/);
-                },
-                success: function (json) {
-                    param.status = true;
+        $.ajax
+        ({
+            type: "GET",
+            url: param.url + param.params,
+            dataType: 'jsonp',
+            name: key,
+            async: true,
+            jsonpCallback: param.callback,
+            //headers:{"Authorization":"Basic ZG9tb3poYWtvX212OjEyMzQ1VGdi"},
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Form " + btoa("domozhako_mv:12345Tgb")/*"Basic ZG9tb3poYWtvX212OjEyMzQ1VGdi"*/);
+            },
+            success: function (json) {
+                param.status = true;
 
-                    (param.return)?param[param.return]=json: param.result = json;
+                (param.return) ? param[param.return] = json : param.result = json;
 
-                    if (param.activeController) {
-                        try {
-                            console.warn("Контроллер " + param.callback + " пошел работать");
-                            param.activeController();
-                        } catch (err) {
-                            console.error("Контроллер " + param.callback + " не существует");
-                        }
-
+                if (param.activeController) {
+                    try {
+                        console.warn("Контроллер " + param.callback + " пошел работать");
+                        param.activeController();
+                    } catch (err) {
+                        console.error("Контроллер " + param.callback + " не существует");
                     }
-                },
-                error: function (json) {
-                            console.error("Ошибка выполнения AJAX "+ param.callback);
-                },
-            });
-        },
+
+                }
+            },
+            error: function (json) {
+                console.error("Ошибка выполнения AJAX " + param.callback);
+            },
+        });
+    },
 
 
-    this.services = {
-        "profile": {
-            "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=empProfile",
-            "status": false,
-            "params": "&user=102838",
-
-            "loadInStart": true,
-            "callback": "getEmpProfile",
-            "activeController": that_.profileController
-        },
-
-        "dict":
-            {
-                "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=dict",
+        this.services = {
+            "profile": {
+                "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=empProfile",
                 "status": false,
                 "params": "&user=102838",
-                "callback": "getDicts",
+
                 "loadInStart": true,
-                "activeController": that_.choiceController
+                "callback": "getEmpProfile",
+                "activeController": that_.profileController
             },
 
-        "instrument": {
-            "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=competentionInstrument",
-            "status": false,
-            "params": "&compitentId=2000001154&user=102838",
-            "loadInStart": false,
-            "callback": "getCompetetionInstrument"
-        },
+            "dict":
+                {
+                    "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=dict",
+                    "status": false,
+                    "params": "&user=102838",
+                    "callback": "getDicts",
+                    "loadInStart": true,
+                    "activeController": that_.choiceController
+                },
 
-        "position": {
-            "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=position",
-            "status": false,
-            "params": "&user=102838&requestType=model&family=[30000047]&row=1_2",
-            "params_":{"user":"102838","requestType":"model","family":"[30000047]","row":"1_40"},
-            "loadInStart": true,
-            "callback": "getRecommendatedPosition",
-            "activeController": that_.positionController
+            "instrument": {
+                "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=competentionInstrument",
+                "status": false,
+                "params": "&compitentId=2000001154&user=102838",
+                "loadInStart": false,
+                "callback": "getCompetetionInstrument"
+            },
 
-        }
-        ,
-        "competention": {
-            "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=positionCompetentions",
-            "status": false,
-            "params": "&user=102838&position=[30007047,30006541]",
-            "loadInStart": true,
-            "callback": "getPositionCompetetions",
-            "activeController": that_.competencesController
-        }
+            "position": {
+                "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=position",
+                "status": false,
+                "params": "&user=102838&requestType=model&family=[30000047]&row=1_2",
+                "params_": {"user": "102838", "requestType": "model", "family": "[30000047]", "row": "1_40"},
+                "loadInStart": true,
+                "callback": "getRecommendatedPosition",
+                "activeController": that_.positionController
 
-    };
+            }
+            ,
+            "competention": {
+                "url": "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=positionCompetentions",
+                "status": false,
+                "params": "&user=102838&position=[30007047,30006541]",
+                "loadInStart": true,
+                "callback": "getPositionCompetetions",
+                "activeController": that_.competencesController
+            }
+
+        };
 
 
     this.initMainBody = function () {
-       var path= $("head").find("link").last().attr("href").split("/");
+
+        var path = $("head").find("link").last().attr("href").split("/");
         path[path.length - 1] = "";
-        this.path=path.join("/");
+        this.path = path.join("/");
         this.refresh();
         var globalSettings = this.globalSettings;
         //============================================================
@@ -131,21 +131,18 @@ function classMainBody() {
         var component = '#' + globalSettings.teg + '_COMPONENT ';
         var html =
             '<div id="sc-app" ng-app="scApp">' +
-            '<main-menu id="id_menu" class="sc-menu"></main-menu>'+
-            '<main id="id_main" class="sc-main enter-active">' +
-                '<div id="swiper-container" class="swiper-container">'+
-                    '<div class="swiper-wrapper">'+
-                        '<div class="swiper-slide">'+
-                            '<div ng-view ></div>'+
-                        '</div>'+
-                    '</div>'+
-                '<div class="swiper-scrollbar"></div>'+
-                '</div>'+
+            '   <main-menu id="id_menu" class="sc-menu"></main-menu>' +
+            '   <main id="id_main" class="sc-main enter-active">' +
+            '   <div id="swiper-container" class="swiper-auto-container">' +
+            '       <div class="swiper-wrapper">' +
+            '           <div class="swiper-slide">' +
+            '               <div ng-view ></div>' +
+            '            </div>' +
+            '           </div>' +
+            //'       <div class="swiper-scrollbar"></div>' +
+            '   </div>' +
             '</main>' +
-          '</div>';
-
-
-
+            '</div>';
 
 
         $(component).append(html);
@@ -154,34 +151,40 @@ function classMainBody() {
             direction: 'vertical',
             slidesPerView: 'auto',
             mousewheel: true,
-            freeMode: false
+            freeMode: true
         });
-
-
-
 
         //============================================================
         //создание основного модуля и роутинга
         //============================================================
-        angular.module('scApp', ["ngRoute","mainMenuModule","choiceModule","profileModule"]);
-        angular.module('scApp').config(function($routeProvider) {
+        angular.module('scApp', ["ngRoute", "mainMenuModule", "choiceModule", "profileModule"]);
+        angular.module('scApp').config(function ($routeProvider) {
             $routeProvider
+                .when("/", {
+                    redirectTo: '/profile'
+                })
                 .when("/profile", {
-                    template : '<profile id="sc-profile" class="profile-slide sc-v-slide"></profile>'
+                    template: '<profile id="sc-profile" class="profile-slide sc-v-slide"></profile>'
                 })
                 .when("/choice", {
-                    template : "<choice></choice>"
+                    template: "<choice></choice>"
                 })
-                .when("/tomato", {
-                    template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
-                });
         });
+
+        angular.module('scApp')
+            .service("swiperService", function() {
+                this.updateSwiper = () => {
+                    that_.profile_swiper.update();
+                }
+            });
+
+
 
         //============================================================
         //регистрация приложения Angular
         //============================================================
         angular.element(function () {
-           angular.bootstrap(document.getElementById("sc-app"), ['scApp']);
-         });
+            angular.bootstrap(document.getElementById("sc-app"), ['scApp']);
+        });
     }
 }
--- Root Folder/_HR/Smart/tech1/modules/scProfile.js
+++ Root Folder/_HR/Smart/tech1/modules/scProfile.js
@@ -4,58 +4,76 @@ function dirProfile() {
         console.warn("dirProfile");
         angular.module('profileModule', [])
 
-            .directive('profile', function ($location,profileSrv) {
+            .directive('profile', function ($location, profileSrv) {
                 return {
                     restrict: 'AE',
-                    scope:{},
-                    templateUrl: that_.path+"modules/profile.html",//that_.profileView(),
-                    controller:  profileController,
+                    scope: {},
+                    templateUrl: that_.path + "modules/profile.html",//that_.profileView(),
+                    controller: profileController,
                     controllerAs: "profile"
                 };
             });
 
 
         angular.module('profileModule')
-            .service("profileSrv", function($http){
-                this.getProfile = function(user){
-                    var _url = "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=empProfileNoCallback&user="+user;
+            .service("profileSrv", function ($http) {
+                this.getProfile = function (user) {
+                    var _url = "https://sbt-surp-216.sigma.sbrf.ru:8292/hr/smartcareer/services/data.xsjs?entity=empProfileNoCallback&user=" + user;
                     var _headers = {
                         'Authorization': "Basic ZG9tb3poYWtvX212OjEyMzQ1VGdi",
                         'Accept': 'application/json; charset=utf-8',
                         'Content-Type': 'application/json; charset=utf-8'
                     };
-                    var promise=  $http({
+                    var promise = $http({
                         method: 'GET',
                         url: _url,
                         headers: _headers
-                    }).then(function(response){
+                    }).then(function (response) {
                         return response.data
+                    }).catch(function (error) {
+                        console.log(error);
                     });
                     return promise
                 };
             });
 
 
-
-        function profileController(profileSrv) {
+        function profileController($scope, $timeout, profileSrv, swiperService) {
             console.warn('profileController');
             this.competencesTypes = ["Corp", "Role", "Func"];
+            this.data = {};
 
 
-
-
-
-            this.range = function(n) {
+            this.range = function (n) {
                 return new Array(n);
             };
 
-
-            profileSrv.getProfile(that_.user).then((data)=>{
-                this.data=data;
-                console.log({"data":data})
+            profileSrv.getProfile(that_.user).then((data) => {
+                this.data = data;
+                $timeout(swiperService.updateSwiper, 0);
+                console.log({"data": data})
             });
 
+            function renderTimelineLine(parent) {
 
+                // Перерисовка линии таймлайна
+                var first_circle = $(parent + " .timeline-circle").first();
+                var last_circle = $(parent + " .timeline-circle").last();
+
+                var line_y_offset = ($(parent + " .timeline-center").first().height() - first_circle.height()) / 2;
+                var line_x_offset = first_circle.position().left + first_circle.width() / 2;
+
+                var line_y1 = first_circle.offset().top;
+                var line_y2 = last_circle.offset().top + last_circle.height();
+
+                var line_height = line_y2 - line_y1;
+
+                $(parent + " .timeline-line").css({
+                    "height": line_height,
+                    "top": 0,
+                    "left": line_x_offset
+                })
+            };
         }
     }());
 }
